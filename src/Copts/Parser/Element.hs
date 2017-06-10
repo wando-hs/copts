24c34ce25c61619e39a682588ce8ab1ccf1c1a3e
Fernando Oliveira <fernando@fholiveira.com>
2017-06-10 16:52:07 -0300

    parse elements

---
+++ src/Copts/Parser/Element.hs
@@ -0,0 +1,48 @@
+module Copts.Parser.Element (Element (..)
+              , Flag (..)
+              , argument
+              , command
+              , option
+              , option'
+              , element
+              ) where
+
+import Text.Megaparsec hiding (option)
+import Control.Applicative
+import Combinators
+
+
+data Flag = Long String | Short Char
+    deriving (Show, Eq)
+
+data Element = Command String | Argument String | Option (Flag, Maybe String)
+    deriving (Show, Eq)
+
+
+name = letterChar <:> (try $ many character)
+    where character = try alphaNumChar <|> oneOf ['-', '_']
+
+parameter = try argument' <|> some upperChar
+
+shortOption = liftA2 (,) flag (optional $ param)
+    where flag = Short <$ string "-" <*> letterChar
+          param = try $ ignore ' ' *> parameter
+
+longOption = liftA2 (,) flag (optional $ param)
+    where flag = Long <$ string "--" <*> name
+          param = try $ ignoreOneOf ['=', ' '] *> parameter
+
+argument' = between (char '<') (char '>') name
+
+command' = try name <|> string "--" <|> string "-"
+
+option' = try shortOption <|> longOption
+
+
+argument = Argument <$> argument'
+
+command = Command <$> command'
+
+option = Option <$> option'
+
+element = tryAll [argument, option, command]
