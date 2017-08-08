e17fa674efc4c486b6869136e4839bba31b38005
Fernando Oliveira <fernando@fholiveira.com>
2017-08-08 09:13:37 -0300

    Rename functios

--- src/Copts/Normalizer.hs
+++ src/Copts/Normalizer.hs
@@ -16,35 +16,34 @@ data Pattern = Command String
              | Repeated Pattern
              deriving (Show, Eq)
 
-normalize'1 :: D.Map String Pattern -> P.Pattern -> Pattern
-normalize'1 d (P.Optional u) = Optional $ normalize' d u
-normalize'1 d (P.Required u) = Required $ normalize' d u
-normalize'1 d (P.Exclusive p) = Exclusive $ map (normalize' d) p
-normalize'1 d (P.Repeated p) = Repeated $ normalize'1 d p
-normalize'1 d P.Options =  Optional . nub . D.elems $ d
-normalize'1 d (P.A (P.Option opt)) = option d $ bimap label id opt
-normalize'1 _ (P.A (P.Command name)) = Command name
-normalize'1 _ (P.A (P.Argument name)) = Argument name
+toPattern :: D.Map String Pattern -> P.Pattern -> Pattern
+toPattern d (P.Optional u) = Optional $ toUsage d u
+toPattern d (P.Required u) = Required $ toUsage d u
+toPattern d (P.Exclusive p) = Exclusive $ map (toUsage d) p
+toPattern d (P.Repeated p) = Repeated $ toPattern d p
+toPattern d P.Options =  Optional . nub . D.elems $ d
+toPattern d (P.A (P.Option opt)) = toOption d $ bimap label id opt
+toPattern _ (P.A (P.Command name)) = Command name
+toPattern _ (P.A (P.Argument name)) = Argument name
 
-option :: D.Map String Pattern -> (String, Maybe String) -> Pattern
-option d (name, param) = if D.member name d
-                            then d D.! name
-                            else Option [name] $ (\ l -> (l, Nothing)) <$> param
+toOption :: D.Map String Pattern -> (String, Maybe String) -> Pattern
+toOption d (name, param) = D.findWithDefault def name d
+    where def = Option [name] $ (\ l -> (l, Nothing)) <$> param
 
-normalize' :: D.Map String Pattern -> P.Usage -> Usage
-normalize' d = map (normalize'1 d)
-
-normalize :: P.Help -> [Usage]
-normalize (P.Simple _ us) = map (normalize' D.empty) us
-normalize (P.Complex _ us options) = map (normalize' $ dict options) us
-    where dict = D.fromList . concat . map details
+toUsage :: D.Map String Pattern -> P.Usage -> Usage
+toUsage d = map (toPattern d)
 
 label :: P.Flag -> String
 label (P.Short name) = [name]
 label (P.Long name) = name
 
-details :: P.OptionDetail -> [(String, Pattern)]
-details (P.Details flags param _) = map (\ l -> (l, opt)) labels
+toDetails :: P.OptionDetail -> [(String, Pattern)]
+toDetails (P.Details flags param _) = map (\ l -> (l, opt)) labels
     where parameter (P.Parameter name value) = (name, value)
           opt = Option labels $ parameter <$> param
           labels = map label flags
+
+normalize :: P.Help -> [Usage]
+normalize (P.Simple _ us) = map (toUsage D.empty) us
+normalize (P.Complex _ us opts) = map (toUsage $ dict opts) us
+    where dict = D.fromList . concat . map toDetails
