0c600faa7aa3c7a187fb0a8fa6b7f610327e0c5b
Fernando Oliveira <fernando@fholiveira.com>
2017-08-08 09:00:28 -0300

    Add command line interface

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,16 @@
 module Main where
 
-main :: IO ()
-main = putStrLn "someFunc"
+
+import Text.Megaparsec (parse, parseMaybe)
+import System.Environment (getArgs)
+
+import Copts.Normalizer (normalize)
+import Copts.Parser (help)
+
+
+options :: [String] -> IO ()
+options ("parse":text:_) = putStrLn . show $ parseMaybe help text
+options ("normalize":text:_) = putStrLn . show $ normalize <$> parseMaybe help text
+options x = mempty
+
+main = options =<< getArgs
