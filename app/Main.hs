fb7616f854c8c1802d62fd0a25d9b6c53870fd1f
Fernando Oliveira <fernando@fholiveira.com>
2017-08-09 18:55:44 -0300

    Create interface for graphs

--- app/Main.hs
+++ app/Main.hs
@@ -3,14 +3,20 @@ module Main where
 
 import Text.Megaparsec (parseMaybe)
 import System.Environment (getArgs)
+import Algebra.Graph (edgeList)
 
+import Copts.Graph (graph)
 import Copts.Normalizer (normalize)
 import Copts.Parser (help)
 
+print' :: Show a => a -> IO ()
+print' = putStrLn . show
 
 options :: [String] -> IO ()
-options ("parse":text:_) = putStrLn . show $ parseMaybe help text
-options ("normalize":text:_) = putStrLn . show $ normalize <$> parseMaybe help text
+options ("parse":text:_) = print' $ parseMaybe help text
+options ("normalize":text:_) = print' $ normalize <$> parseMaybe help text
+options ("graph":text:args) = print' $ map (edgeList . graph) <$> normalize <$> parseMaybe help text
+
 options x = mempty
 
 main = options =<< getArgs
